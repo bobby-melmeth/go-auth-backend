65e60d68925f793fb2d71e68278e55881178191c
bobbyPischer <robmelmeth@gmail.com>
2022-11-13 12:54:02 +1000

    adds .env file

--- go-auth/database/connection.go
+++ go-auth/database/connection.go
@@ -1,7 +1,11 @@
 package database
 
 import (
+	"log"
+	"os"
+
 	"github.com/bobbyMoonward/go-auth-backend/models"
+	"github.com/joho/godotenv"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
@@ -9,7 +13,11 @@ import (
 	var DB *gorm.DB
 
 	func Connect(){
-	dsn := "host=localhost user=postgres password=Petroski1 dbname=go-auth port=5432 sslmode=disable TimeZone=Asia/Shanghai"
+		err := godotenv.Load()
+		if err != nil {
+			log.Fatal("Error loading .env file")
+		}
+	dsn := os.Getenv("DB_CREDENTIALS")
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 
 	if err != nil {
